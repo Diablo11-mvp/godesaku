ac55003bb0e50a1bb3c313691e63a7236bd15fb8
fizki <fizkifajrul@gmail.com>
2026-01-11 16:28:06 +0700

    first init github

---
+++ kecamatan/data/balai_kota_4.js
@@ -0,0 +1,1 @@
+var json_balai_kota_4 = {"type":"FeatureCollection","name":"balai_kota_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"nama":"Balai Kota Kec. Bumiayu"},"geometry":{"type":"Point","coordinates":[109.004905422652769,-7.252618841310237]}}]}
